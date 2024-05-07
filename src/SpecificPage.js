3dee138b511287c5ec9adc1d9f2aaaedc9a41d34
njeri <njeri255mungai@gmail.com>
2024-05-07 21:23:27 +0300

    initial commit

--- src/SpecificPage.js
+++ src/SpecificPage.js
@@ -1,12 +1,28 @@
-function SpecificPage() {
+import React, { useState } from 'react';
 
-    return(
-        <div>
-            
-            
+const SpecificPage = ({ image, description, plansofthepackage, price, onBookNow, onAddToWishlist }) => {
+  const [isWishlist, setIsWishlist] = useState(false);
 
-        </div>
-    )
-    
-}
+  const handleBookNow = () => {
+    onBookNow();
+  };
+
+  const handleAddToWishlist = () => {
+    setIsWishlist(!isWishlist);
+
+    onAddToWishlist();
+  };
+
+  return (
+    <div className="travel-package">
+      <img src="https://i.pinimg.com/564x/ad/37/05/ad370511d29ac73fc9b0d9330deeb4d4.jpg" alt="Travel destination" />
+      <p>{description}</p>
+      <p>{plansofthepackage}</p>
+      <p>Price: {price}</p>
+      <button onClick={handleBookNow}>Book Now</button>
+      <button onClick={handleAddToWishlist}>{isWishlist ? "Remove from Wishlist" : "Add to Wishlist"}</button>
+    </div>
+  );
+
+};
 export default SpecificPage
