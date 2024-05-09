6caef77634d5022df34ffa36c02caa32c023e144
Telvin <mark.telvin@student.moringaschool.com>
2024-05-09 10:45:31 +0300

    tj

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -2,9 +2,9 @@ import React, { useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './WishlistPage.module.css';
-
+import { useNavigate } from 'react-router-dom';
 const WishlistPage = () => {
- 
+ const navigate = useNavigate ()
   const [wishlistItems, setWishlistItems] = useState([
     { id: 1, imageUrl: 'https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg', name: '4-day Adventure in Bali' },
     { id: 2, imageUrl: 'https://media.timeout.com/images/105240236/image.jpg', name: '6-day Gastronomic and Scuba Diving Adventure in Thailand' },
@@ -12,14 +12,6 @@ const WishlistPage = () => {
   ]);
 
  
-  const addItemToWishlist = () => {
-    const newItem = {
-      id: wishlistItems.length + 1,
-      imageUrl: `image${wishlistItems.length + 1}.jpg`,
-      name: `Item ${wishlistItems.length + 1}`,
-    };
-    setWishlistItems([...wishlistItems, newItem]);
-  };
 
  
   const removeItemFromWishlist = (id) => {
@@ -30,11 +22,11 @@ const WishlistPage = () => {
   return (
     <div className={styles['wishlist-page']}> {/* Use CSS module class */}
       <h1 className={styles['page-title']}>Wishlist Page</h1> {/* Use CSS module class */}
-      <button onClick={addItemToWishlist}>Add Item</button>
+      
       <Carousel showThumbs={false} infiniteLoop autoPlay>
         {wishlistItems.map((item) => (
           <div key={item.id}>
-            <img src={item.imageUrl} alt={item.name} />
+            <img src={item.imageUrl} alt={item.name}  onClick={()=>navigate(`/getway-travels/specificPage/${item.id}`)} />
             <p className="legend">{item.name}</p>
             <button onClick={() => removeItemFromWishlist(item.id)}>Remove</button>
           </div>
