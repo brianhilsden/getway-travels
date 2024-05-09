00589a572d716f6177c171e03a1a1791ffac09e1
Brian Omondi <brianhilsden@gmail.com>
2024-05-09 14:40:13 +0300

    added nav styling

--- src/Wishlist.js
+++ src/Wishlist.js
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Carousel } from 'react-responsive-carousel';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import styles from './WishlistPage.module.css';
 import { useNavigate } from 'react-router-dom';
+import { useOutletContext } from 'react-router-dom';
 const WishlistPage = () => {
  const navigate = useNavigate ()
-  const [wishlistItems, setWishlistItems] = useState([
-    { id: 1, imageUrl: 'https://a.cdn-hotels.com/gdcs/production143/d1112/c4fedab1-4041-4db5-9245-97439472cf2c.jpg', name: '4-day Adventure in Bali' },
-    { id: 2, imageUrl: 'https://media.timeout.com/images/105240236/image.jpg', name: '6-day Gastronomic and Scuba Diving Adventure in Thailand' },
-    { id: 3, imageUrl: 'https://images.squarespace-cdn.com/content/v1/57265e5745bf21105b61fddb/1704323010416-LLF3J3YUHPL648IOUGG6/089A6147.png?format=500w', name: 'Paris 5 Day trip' },
-  ]);
+  const [wishlistItems, setWishlistItems] = useState([]);
+  const [ , , ,setShowSearchBar] = useOutletContext()
 
- 
+ useEffect(()=>{
+  fetch("http://localhost:4001/wishlist")
+  .then(res=>res.json())
+  .then(data=>setWishlistItems(data))
+  setShowSearchBar(false)
+
+ },[])
 
  
   const removeItemFromWishlist = (id) => {
     const updatedWishlistItems = wishlistItems.filter((item) => item.id !== id);
     setWishlistItems(updatedWishlistItems);
+    fetch(`http://localhost:4001/wishlist/${id}`,{
+      method:"DELETE",
+      headers:{
+        "Content-Type":"application/json",
+        "Accept":"application/json"
+      }
+      
+    })
   };
 
   return (
     <div className={styles['wishlist-page']}> {/* Use CSS module class */}
-      <h1 className={styles['page-title']}>Wishlist Page</h1> {/* Use CSS module class */}
+     
       
       <Carousel showThumbs={false} infiniteLoop autoPlay>
         {wishlistItems.map((item) => (
           <div key={item.id}>
-            <img src={item.imageUrl} alt={item.name}  onClick={()=>navigate(`/getway-travels/specificPage/${item.id}`)} />
-            <p className="legend">{item.name}</p>
-            <button onClick={() => removeItemFromWishlist(item.id)}>Remove</button>
+            <img src={item.image} alt={item.name} style={{height:"100vh"}}   />
+            <p className="legend">{item.name}<button onClick={()=>navigate(`/getway-travels/specificPage/${item.id}`)}>View details</button>  <button className='btn btn-secondary ms-3' onClick={() => removeItemFromWishlist(item.id)}>Remove from wishlist</button></p>
+          
           </div>
         ))}
       </Carousel>
--- src/routes.js
+++ src/routes.js
@@ -5,15 +5,26 @@ import SpecificPage from "./SpecificPage";
 import ContactUs from "./ContactUs";
 import FeedBackForm from "./FeedbackForm";
 import Wishlist from "./Wishlist";
+import LandingPage from "./LandingPage";
 
 const routes = [
-  { path: "/getway-travels", element: <App /> },
-  { path: "/getway-travels/specificPage/:id", element: <SpecificPage /> },
-  { path: "/getway-travels/signUp", element: <SignUp /> },
-  { path: "/getway-travels/login", element: <Login /> },
-  { path: "/getway-travels/contactUs", element: <ContactUs /> },
-  { path: "/getway-travels/signUp", element: <SignUp /> },
-  { path: "/getway-travels/feedbackForm", element: <FeedBackForm /> },
-  { path: "/getway-travels/wishlist", element: <Wishlist /> },
+  { path: "/getway-travels",
+   element: <App />,
+    children:[
+      {
+        path:"/getway-travels",
+        element:<LandingPage/>
+      },
+      { path: "/getway-travels/specificPage/:id", element: <SpecificPage /> },
+      { path: "/getway-travels/signUp", element: <SignUp /> },
+      { path: "/getway-travels/login", element: <Login /> },
+      { path: "/getway-travels/contactUs", element: <ContactUs /> },
+      { path: "/getway-travels/signUp", element: <SignUp /> },
+      { path: "/getway-travels/feedbackForm", element: <FeedBackForm /> },
+      { path: "/getway-travels/wishlist", element: <Wishlist /> },
+
+    ]
+  },
+
 ];
 export { routes };
