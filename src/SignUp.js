259cfd40220337dccca63d3a2fc4447ab058e988
Brian Omondi <brianhilsden@gmail.com>
2024-05-09 16:17:36 +0300

    pushed to ranch

--- src/SignUp.js
+++ src/SignUp.js
@@ -28,28 +28,8 @@ function SignUpForm() {
 
 
   return (
-
-    <>
-    <div className="form-container" >
-      <h2>Sign Up</h2>
-      <form onSubmit={handleSubmit} >
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" value={formData.username} onChange={handleInputChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="email" id="email" name="email" value={formData.email} onChange={handleInputChange} />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" value={formData.password} onChange={handleInputChange} />
-        </div>
-        <button type="submit">Sign Up</button>
-      </form>
-
-    <div className="container">
-      <div className="background"></div>
+    <div className="container-fluid">
+      <div className="background-signUp" ></div>
      <div className="form-card">
 
         <h2>Sign Up</h2>
@@ -69,9 +49,7 @@ function SignUpForm() {
           <button type="submit">Sign Up</button>
         </form>
       </div>
-
     </div>
-    </>
   );
   }
 
