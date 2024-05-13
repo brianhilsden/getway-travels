73a7e4efcca4053b08755d55598427402b276654
njeri <njeri255mungai@gmail.com>
2024-05-13 09:48:58 +0300

    initial commit

--- src/SpecificPage.js
+++ src/SpecificPage.js
@@ -47,6 +47,8 @@ const SpecificPage = ({name, image, description, Itinerary, price, onBookNow, on
         
       }).then(()=>setIsWishlist(false))}}
   return (
+
+    
     <div className="container-fluid" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1471922694854-ff1b63b20054?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
       <h1 className='text-center'>{SpecificPage.name}</h1>
       <div className='d-flex align-items-center'>
