fb467e91f3db00c207b63d414efde3a57823c136
Brian Omondi <brianhilsden@gmail.com>
2024-05-13 10:44:58 +0300

    updated styles

--- src/SpecificPage.js
+++ src/SpecificPage.js
@@ -59,7 +59,7 @@ const SpecificPage = () => {
       }).then(()=>setSpecificPage(prev=>({...prev,wishlist:false})))}}
   return (
     <div className="container-fluid" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1471922694854-ff1b63b20054?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')` }}>
-      <div style={{height:"13vh"}}></div>
+      <div style={{height:"9.3vh"}}></div>
       <h1 className='text-center'>{SpecificPage.name}</h1>
       <div className='d-flex align-items-center'>
       <img src={SpecificPage.image} width="100%" height="50vh" className='ms-2' alt="Travel destination" style={{ top: 0, left: 0, order: -1, width: '100%', height: '50vh' }} />
